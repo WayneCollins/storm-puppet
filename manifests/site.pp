5cafec382ced7564ee36b10ad94dc601dc98b4f2
WayneCollins <wayne.collins@gfsa.com>
2014-08-18 13:09:02 -0400

    fix name probs

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,14 @@
-node 'bwc.nimbus' {
+node 'nimbus' {
   $cluster = 'storm1'
   include storm::nimbus
   include storm::ui
 }
 
-node /bwc.supervisor[1-9]/ {
+node /supervisor[1-9]/ {
   $cluster = 'storm1'
   include storm::supervisor
 }
 
-node /bwc.zookeeper[1-9]/ {
+node /zookeeper[1-9]/ {
   include storm::zoo
 }
